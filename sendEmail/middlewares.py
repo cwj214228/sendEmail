836592634bba7616e506df170de0b2aedf2b1ffe
cwj214228 <1343500013@qq.com>
2019-03-27 20:43:13 +0800

    update

--- sendEmail/middlewares.py
+++ sendEmail/middlewares.py
@@ -1,11 +1,49 @@
 # -*- coding: utf-8 -*-
-
+import random
+import base64
+# 导入settings的PROXIES设置
+from settings import PROXIES
 # Define here the models for your spider middleware
 #
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
 from scrapy import signals
+
+
+# 随机使用预定义列表里的 User-Agent类
+class RandomUserAgent(object):
+    def __init__(self, agents):
+        # 使用初始化的agents列表
+        self.agents = agents
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # 获取settings的USER_AGENT列表并返回
+        return cls(crawler.settings.getlist('USER_AGENTS'))
+
+    def process_request(self, request, spider):
+        # 随机设置Request报头header的User-Agent
+        request.headers.setdefault('User-Agent', random.choice(self.agents))
+
+
+
+# 随机使用预定义列表里的 Proxy代理
+class ProxyMiddleware(object):
+    def process_request(self, request, spider):
+        # 随机获取from settings import PROXIES里的代理
+        proxy = random.choice(PROXIES)
+
+        # 如果代理可用，则使用代理
+        if proxy['user_pass'] is not None:
+            request.meta['proxy'] = "http://%s" % proxy['ip_port']
+            # 对代理数据进行base64编码
+            encoded_user_pass = base64.encodestring(proxy['user_pass'])
+            # 添加到HTTP代理格式里
+            request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
+        else:
+            print ("****代理失效****" + proxy['ip_port'])
+            request.meta['proxy'] = "http://%s" % proxy['ip_port']
 
 
 class SendemailSpiderMiddleware(object):
--- sendEmail/settings.py
+++ sendEmail/settings.py
@@ -16,7 +16,23 @@ NEWSPIDER_MODULE = 'sendEmail.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'sendEmail (+http://www.yourdomain.com)'
+USER_AGENT = [
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+    "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+    "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5"
+]
+
+PROXIES = [
+    {'ip_port': '58.243.56.148:8060', 'user_pass': ''},
+    {'ip_port': '27.188.62.55:8060', 'user_pass': ''},
+    {'ip_port': '117.184.67.118:8060', 'user_pass': ''},
+    {'ip_port': '60.249.176.76:8080', 'user_pass': ''},
+]
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
@@ -27,13 +43,14 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+# 设置下载延迟
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -52,9 +69,11 @@ ROBOTSTXT_OBEY = False
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'sendEmail.middlewares.SendemailDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   'sendEmail.middlewares.SendemailDownloaderMiddleware': 543,
+    'sendEmail.middlewares.RandomUserAgent': 1,
+    'sendEmail.middlewares.ProxyMiddleware': 100
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
